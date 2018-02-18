44c31843e2c0c700722ac5d3161e965e7b82ade4
rohit9075 <rohit9075@gmail.com>
2018-02-18 10:56:11 +0530

    Initial commit

---
+++ app/src/main/java/rohit/com/fragmentdialog/MainActivity.java
@@ -0,0 +1,31 @@
+package rohit.com.fragmentdialog;
+
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDialogFragment dialogFrag = new MyDialogFragment();
+                FragmentManager fm = getFragmentManager();
+                dialogFrag.show(fm, getString(R.string.dialog_tag));
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/rohit/com/fragmentdialog/MyDialogFragment.java
@@ -0,0 +1,178 @@
+package rohit.com.fragmentdialog;
+
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
+/**
+ * Created by rohit on 14-12-2017.
+ */
+
+public class MyDialogFragment  extends DialogFragment {
+
+    private final String LOG_TAG = MyDialogFragment.class.getSimpleName();
+
+
+    // onCreate --> (onCreateDialog) --> onCreateView --> onActivityCreated
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
+        Log.v(LOG_TAG, "onCreateView");
+
+        View dialogView = inflater.inflate(R.layout.dialog_content, container, false);
+
+
+        final EditText editText = dialogView.findViewById(R.id.editText);
+
+        final SpeechRecognizer mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(getActivity());
+
+
+        final Intent mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault());
+
+
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                //getting all the matches
+                ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                //displaying the first match
+                if (matches != null)
+                    editText.setText(matches.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+
+
+
+
+        dialogView.findViewById(R.id.button).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        mSpeechRecognizer.stopListening();
+                        editText.setHint("You will see input here");
+                        break;
+
+                    case MotionEvent.ACTION_DOWN:
+                        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                        editText.setText("");
+                        editText.setHint("Listening...");
+                        break;
+                }
+                return false;
+            }
+        });
+
+
+
+
+
+
+
+
+        return dialogView;
+    }
+
+    // If shown as dialog, set the width of the dialog window
+    // onCreateView --> onActivityCreated -->  onViewStateRestored --> onStart --> onResume
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.v(LOG_TAG, "onResume");
+        if (getShowsDialog()) {
+            // Set the width of the dialog to the width of the screen in portrait mode
+            DisplayMetrics metrics = getActivity().getResources().getDisplayMetrics();
+            int dialogWidth = Math.min(metrics.widthPixels, metrics.heightPixels);
+            getDialog().getWindow().setLayout(dialogWidth, WRAP_CONTENT);
+        }
+    }
+
+    private void showToast(String buttonName) {
+        Toast.makeText(getActivity(), "Clicked on \"" + buttonName + "\"", Toast.LENGTH_SHORT).show();
+    }
+
+    // If dialog is cancelled: onCancel --> onDismiss
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        Log.v(LOG_TAG, "onCancel");
+    }
+
+    // If dialog is cancelled: onCancel --> onDismiss
+    // If dialog is dismissed: onDismiss
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        Log.v(LOG_TAG,"onDismiss");
+    }
+
+
+
+
+
+
+
+}
